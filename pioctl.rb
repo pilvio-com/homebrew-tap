c9f15357700f451d4420688da60f2012ca213cf1
vtarmo <tarmo@rebahn.eu>
2022-11-05 12:38:23 +0200

    Brew formula update for pioctl version v0.3.2

--- pioctl.rb
+++ pioctl.rb
@@ -5,20 +5,20 @@
 class Pioctl < Formula
   desc ""
   homepage "https://github.com/pilvio-com/pioctl"
-  version "0.3.1"
+  version "0.3.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/pilvio-com/pioctl/releases/download/v0.3.1/pioctl_0.3.1_darwin_arm64.tar.gz"
-      sha256 "c627d1c6cfdd5d55dcef2c1cdb50c8916939b6ef44f1badaf082d5b859930487"
+      url "https://github.com/pilvio-com/pioctl/releases/download/v0.3.2/pioctl_0.3.2_darwin_arm64.tar.gz"
+      sha256 "cb6e73e9c993ce7816dd5ba4492b25d36f76c59187dd8ed0f4353bd0508d82d2"
 
       def install
         bin.install "pioctl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/pilvio-com/pioctl/releases/download/v0.3.1/pioctl_0.3.1_darwin_amd64.tar.gz"
-      sha256 "612a512f60fbed565ffad20703972e6fe33bb82ce49d3a36effca616808eb2c7"
+      url "https://github.com/pilvio-com/pioctl/releases/download/v0.3.2/pioctl_0.3.2_darwin_amd64.tar.gz"
+      sha256 "faa383fc6bab8631920a90dafa8c7cbf690a8c3c19d0bd39c86978cd6381070d"
 
       def install
         bin.install "pioctl"
@@ -28,16 +28,16 @@ class Pioctl < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pilvio-com/pioctl/releases/download/v0.3.1/pioctl_0.3.1_linux_arm64.tar.gz"
-      sha256 "7ba70aa9738486c3589fca0120d7e19076fe07c8049d81ebb70e090753ea4713"
+      url "https://github.com/pilvio-com/pioctl/releases/download/v0.3.2/pioctl_0.3.2_linux_arm64.tar.gz"
+      sha256 "59cccdd1012a7cb3bbed2c80dcabbda314f98debd137f0f8199b0b773eb57b8c"
 
       def install
         bin.install "pioctl"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/pilvio-com/pioctl/releases/download/v0.3.1/pioctl_0.3.1_linux_amd64.tar.gz"
-      sha256 "e0908db3f6e2d233f7b104908c040340f125a409c6eca8d550b2355f3ae5e0d2"
+      url "https://github.com/pilvio-com/pioctl/releases/download/v0.3.2/pioctl_0.3.2_linux_amd64.tar.gz"
+      sha256 "b998f212b97f335ceb1a67f23f26a04d9a56d662c149a7d92b8caa74d75d72ab"
 
       def install
         bin.install "pioctl"
