748089b5bed119922373a4f0eaef84500673118f
vtarmo <tarmo@rebahn.eu>
2022-12-27 15:31:08 +0200

    Brew formula update for pioadmin version v0.1.3-rc6

--- pioadmin.rb
+++ pioadmin.rb
@@ -5,11 +5,11 @@
 class Pioadmin < Formula
   desc ""
   homepage "https://github.com/pilvio-com/pioadmin"
-  version "0.1.3-rc5"
+  version "0.1.3-rc6"
 
   on_macos do
-    url "https://github.com/pilvio-com/pioadmin/releases/download/v0.1.3-rc5/pioadmin_0.1.3-rc5_darwin_all.tar.gz"
-    sha256 "0a7cc2ed07d3d7846a9f11743441d2b6107e042f4f41eb7f0dddd1988d644b35"
+    url "https://github.com/pilvio-com/pioadmin/releases/download/v0.1.3-rc6/pioadmin_0.1.3-rc6_darwin_all.tar.gz"
+    sha256 "229a9695035ddb7a6b5f82fe4a846531f1baecfb26c6a8841974cf0c33a67fe2"
 
     def install
       bin.install "pioadmin"
@@ -18,16 +18,16 @@ class Pioadmin < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pilvio-com/pioadmin/releases/download/v0.1.3-rc5/pioadmin_0.1.3-rc5_linux_arm64.tar.gz"
-      sha256 "860f397bed8fca147b02d8eceb0821a0febe0fdb5a20ccf507cfb21571e82c13"
+      url "https://github.com/pilvio-com/pioadmin/releases/download/v0.1.3-rc6/pioadmin_0.1.3-rc6_linux_arm64.tar.gz"
+      sha256 "19cac435aecf992156f3dd67b9a756a3a71c424a6c4667495d7e876f2c82383b"
 
       def install
         bin.install "pioadmin"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/pilvio-com/pioadmin/releases/download/v0.1.3-rc5/pioadmin_0.1.3-rc5_linux_amd64.tar.gz"
-      sha256 "1c12b47ea6b6427398efabec2bdcd221677be5b6318678647a07a6d0527948ca"
+      url "https://github.com/pilvio-com/pioadmin/releases/download/v0.1.3-rc6/pioadmin_0.1.3-rc6_linux_amd64.tar.gz"
+      sha256 "6dade7e72fe97a5dd8ebccaf6cc921df627fe1feb61ec7e29320779032dae65a"
 
       def install
         bin.install "pioadmin"
